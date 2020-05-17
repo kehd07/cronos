1c5ed568efef00cc54781e5ea8d8d1d5614804e9
Luis <kehd07@gmail.com>
2020-05-17 14:22:25 +0200

    Add mongod

--- server.js
+++ server.js
@@ -7,27 +7,36 @@ import express from 'express';
 
 const app = express();
 const cors = require('cors');
+const Mongod = require('mongod');
+const server = new Mongod(27017);
 
-const mongoUrl = 'mongodb://localhost:27017';
-const options = {useNewUrlParser: true, useUnifiedTopology: true};
+server.open((err) => {
+  if (err === null) {
+    const mongoUrl = 'mongodb://localhost:27017';
+    const options = {useNewUrlParser: true, useUnifiedTopology: true};
 
-const mongoClientPromise = MongoClient.connect(mongoUrl, options);
-const mongoDbPromise = mongoClientPromise.then((client) => client.db('cronos'));
+    const mongoClientPromise = MongoClient.connect(mongoUrl, options);
+    const mongoDbPromise = mongoClientPromise
+        .then((client) => client.db('cronos'));
 
-const root = {client: mongoClientPromise, db: mongoDbPromise};
-const executableSchema = makeExecutableSchema({typeDefs: schema, resolvers});
+    const root = {client: mongoClientPromise, db: mongoDbPromise};
+    const executableSchema = makeExecutableSchema(
+        {typeDefs: schema, resolvers},
+    );
 
-app.use(
-    '/graphql',
-    cors(),
-    expressGraphql({
-      schema: executableSchema,
-      graphiql: true,
-      rootValue: root,
-    }),
-);
+    app.use(
+        '/graphql',
+        cors(),
+        expressGraphql({
+          schema: executableSchema,
+          graphiql: true,
+          rootValue: root,
+        }),
+    );
 
-const port = 3000;
-app.listen(port, function() {
-  console.log('Server ready at http://localhost:' + port + '/graphql');
+    const port = 3000;
+    app.listen(port, function() {
+      console.log('Server ready at http://localhost:' + port + '/graphql');
+    });
+  }
 });
